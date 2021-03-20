142a6320070d13b5cf71d7d6756a062a35671e1e
Nodarra <sisicdino4@gmail.com>
2021-03-20 21:56:42 +0100

    Second commit, Header added

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import './Styles/App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      {/* Tinder cards */}
+      {/* Swipebutton */}
+    </div>
+  );
+}
+
+export default App;
